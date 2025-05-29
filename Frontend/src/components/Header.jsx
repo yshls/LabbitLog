df7dd46f11c5b59460aace23d4a1640fd4946e9f
yshls <shluxnsal01@gmail.com>
2025-05-30 01:13:34 +0900

    feat: logo 만들기

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,14 +4,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { setUserInfo } from '../store/userSlice'
 import { getUserProfile, logoutUser } from '../apis/userApi'
+import logo from '../assets/logo.png'
 
 export const Header = () => {
-  const [isMenuActive, setIsMenuActive] = useState(false)
-
   const dispatch = useDispatch()
   const user = useSelector(state => state.user.user)
   const username = user?.username
   console.log(username)
+
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -36,7 +36,6 @@ export const Header = () => {
     try {
       await logoutUser()
       dispatch(setUserInfo(''))
-      setIsMenuActive(false)
     } catch (err) {
       console.log('프로필 조회 실패:', err)
       // 401 에러는 로그인 필요 상태이므로 정상 처리
@@ -56,73 +55,54 @@ export const Header = () => {
     )
   }
 
-  const toggleMenu = () => {
-    setIsMenuActive(prev => !prev)
-  }
-  const closeMenu = () => {
-    setIsMenuActive(false)
-  }
-  // 배경 영역(gnbCon)만 클릭 시 닫히도록 하는 핸들러
-  const handleBackgroundClick = e => {
-    // 클릭된 요소가 css.gnbCon 클래스를 가진 요소와 동일할 때만 closeMenu 실행
-    if (e.target === e.currentTarget) {
-      closeMenu()
-    }
-  }
-
-  // gnb 영역 클릭 시 이벤트 전파 중단
-  const handleGnbClick = e => {
-    e.stopPropagation()
-  }
   return (
-    <header className={css.header}>
-      <h1>
-        <Link to={'/'}>LabbitLog</Link>
+    <aside className={css.sidebar}>
+      <h1 className={css.logo}>
+        <Link to="/">
+          <img src={logo} alt="LabbitLog Logo" />
+        </Link>
       </h1>
-      <Hamburger isMenuActive={isMenuActive} toggleMenu={toggleMenu} />
-      <nav className={css.gnbCon} onClick={handleBackgroundClick}>
-        <div className={css.gnb} onClick={handleGnbClick}>
+      {isLoading ? (
+        <p>로딩 중...</p>
+      ) : (
+        <nav className={css.nav}>
           {username ? (
             <>
-              <MenuLike to="/createPost" label="글쓰기" closeMenu={closeMenu} />
+              <MenuLike to="/createPost" label="글쓰기" />
               <MenuLike
                 to={`/mypage/${username}`}
-                label={`마이페이지(${username})`}
-                closeMenu={closeMenu}
+                label="마이페이지"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                  </svg>
+                }
               />
-
-              <button onClick={handleLogout}>로그아웃</button>
+              <button onClick={handleLogout} className={css.logoutBtn}>
+                로그아웃
+              </button>
             </>
           ) : (
             <>
-              <MenuLike to="/register" label="회원가입" closeMenu={closeMenu} />
-              <MenuLike to="/login" label="로그인" closeMenu={closeMenu} />
+              <MenuLike to="/register" label="회원가입" />
+              <MenuLike to="/login" label="로그인" />
             </>
           )}
-        </div>
-      </nav>
-    </header>
+        </nav>
+      )}
+    </aside>
   )
 }
 
-const MenuLike = ({ to, label, closeMenu }) => (
-  <NavLink to={to} className={({ isActive }) => (isActive ? css.active : '')} onClick={closeMenu}>
+const MenuLike = ({ to, label }) => (
+  <NavLink to={to} className={({ isActive }) => (isActive ? css.active : '')}>
     {label}
   </NavLink>
-)
-
-const Hamburger = ({ isMenuActive, toggleMenu }) => (
-  <button className={`${css.hamburger} ${isMenuActive ? css.active : ''}`} onClick={toggleMenu}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      className="bi bi-list"
-      viewBox="0 0 16 16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-      />
-    </svg>
-  </button>
 )
