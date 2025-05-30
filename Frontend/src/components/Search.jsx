22427bedd8ad1d32c31ccb355475826826603c6e
yshls <shluxnsal01@gmail.com>
2025-05-31 01:34:31 +0900

    fix: searchBar 디자인 변경

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -1,11 +1,11 @@
 import { FiSearch } from 'react-icons/fi'
 import css from './search.module.css'
 
-export const Search = ({ value, onChange, placeholder = '검색어를 입력하세요' }) => {
+export const Search = ({ value, onChange, placeholder = '검색' }) => {
   return (
     <div className={css.searchbar}>
-      <input type="text" value={value} onChange={onChange} placeholder={placeholder} />
       <FiSearch className={css.icon} />
+      <input type="text" value={value} onChange={onChange} placeholder={placeholder} />
     </div>
   )
 }
