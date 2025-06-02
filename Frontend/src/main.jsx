05d6337d541088fd32e392fdaa0c0da27f9d8f52
yshls <shluxnsal01@gmail.com>
2025-06-02 09:40:56 +0900

    feat: 다시 삭제

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,14 +4,11 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <SpeedInsights projectId="labbitlog">
-        <RouterProvider router={router} />
-      </SpeedInsights>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 )
