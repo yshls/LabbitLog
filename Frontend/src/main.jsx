0c23e4c24e56439cc6ff61da3b618302b0cacfa4
yshls <shluxnsal01@gmail.com>
2025-06-02 09:37:38 +0900

    feat: 성능 분석 설치

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <SpeedInsights projectId="labbitlog">
+        <RouterProvider router={router} />
+      </SpeedInsights>
     </Provider>
   </StrictMode>
 )
