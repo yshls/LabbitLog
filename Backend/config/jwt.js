34cb04b0bf4dbd49dafadbf398f8bed0bda98b0d
yshls <shluxnsal01@gmail.com>
2025-06-03 10:22:43 +0900

    feat: jwt.js 크로스 도메인 설정

--- Backend/config/jwt.js
+++ Backend/config/jwt.js
@@ -7,7 +7,7 @@ export const tokenLife = process.env.JWT_EXPIRATION;
 export const cookieOptions = {
   httpOnly: true,
   maxAge: 1000 * 60 * 60, // 1시간
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  secure: true, // HTTPS 환경에서만 작동하도록 설정
+  sameSite: 'none', // 크로스 도메인 요청 허용
   path: '/',
 };
